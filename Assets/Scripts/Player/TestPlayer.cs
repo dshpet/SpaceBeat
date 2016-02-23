444c07aa37ea4dd533c30a9b3e2962f59de0db31
dshpet <dshpet@gmail.com>
2016-02-23 15:06:38 +0200

    add player, scene, camera for algorithm test

---
+++ Assets/Scripts/Player/TestPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayer : MonoBehaviour {
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
