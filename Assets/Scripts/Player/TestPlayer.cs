161ee0a0e5f1278130801d1e4ecb0f1b6101644c
dshpet <dshpet@gmail.com>
2016-02-24 21:41:07 +0200

    add dummy real time analyzer

--- Assets/Scripts/Player/TestPlayer.cs
+++ Assets/Scripts/Player/TestPlayer.cs
@@ -12,9 +12,35 @@ public class TestPlayer : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        var spectrum = new float[1024];
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
 
-        transform.Translate(0, 0, speed * Time.deltaTime);
-        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        float influence = 0;
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            if (i < spectrum.Length / 3)
+                influence += spectrum[i] * 0.5f;
+            else
+            if (i >= spectrum.Length / 3 && i <= spectrum.Length * 2 / 3)
+                influence += spectrum[i] * 0.3f;
+            else
+            if (i > spectrum.Length * 2 / 3)
+                influence += spectrum[i] * 0.2f;
+        }
+
+        /*
+        var c1 = spectrum[2] + spectrum[3] + spectrum[4];
+        var c3 = spectrum[11] + spectrum[12] + spectrum[13];
+        var c4 = spectrum[22] + spectrum[23] + spectrum[24];
+        var c5 = spectrum[44] + spectrum[45] + spectrum[46] + spectrum[47] + spectrum[48] + spectrum[49];
+
+        Debug.Log("C1 = " + c1);
+        var influence = (float) (c1 * 0.1 + c3 * 0.3 + c4 * 0.5 + c5 * 0.1);
+        */
+
+        transform.Translate(0, 0, influence * 50 * Time.deltaTime);
+        //transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
